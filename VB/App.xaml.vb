afae302c2cb9e92e6dfb223b677c2d35b3a9c8ce
DevExpressExampleBot <fakeEmail>
2026-02-04 03:40:24 +0400

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to enable Drag and Drop feature in the TableView
 ' 
 ' This example shows how to enable Drag and Drop functionality in the
@@ -10,19 +10,14 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E3918
-
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
 Imports System.Windows
 
 Namespace How_to_Drag_and_Drop_Between_GridControl_and_Other_Controls
+
     ''' <summary>
     ''' Interaction logic for App.xaml
     ''' </summary>
-    Partial Public Class App
+    Public Partial Class App
         Inherits Application
 
     End Class
--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Collections.ObjectModel
 Imports System.Windows.Controls
 Imports System.Collections.Generic
@@ -8,14 +8,20 @@ Imports System.Windows.Media
 Imports DevExpress.Xpf.Core
 
 Namespace How_to_Drag_and_Drop_Between_GridControl_and_Other_Controls
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Class Employee
-            Public Property ID() As Integer
-            Public Property Name() As String
-            Public Property Position() As String
-            Public Property Department() As String
+
+            Public Property ID As Integer
+
+            Public Property Name As String
+
+            Public Property Position As String
+
+            Public Property Department As String
+
             Public Overrides Function ToString() As String
                 Return Name
             End Function
@@ -23,143 +29,45 @@ Namespace How_to_Drag_and_Drop_Between_GridControl_and_Other_Controls
 
         Public NotInheritable Class Stuff
 
-            Private Sub New()
-            End Sub
-
             Public Shared Function GetStuff() As ObservableCollection(Of Employee)
-
-                Dim stuff_Renamed As New ObservableCollection(Of Employee)()
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 1, _
-                    .Name = "Gregory S. Price", _
-                    .Department = "", _
-                    .Position = "President" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 2, _
-                    .Name = "Irma R. Marshall", _
-                    .Department = "Marketing", _
-                    .Position = "Vice President" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 3, _
-                    .Name = "John C. Powell", _
-                    .Department = "Operations", _
-                    .Position = "Vice President" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 4, _
-                    .Name = "Christian P. Laclair", _
-                    .Department = "Production", _
-                    .Position = "Vice President" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 5, _
-                    .Name = "Karen J. Kelly", _
-                    .Department = "Finance", _
-                    .Position = "Vice President" _
-                })
-
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 6, _
-                    .Name = "Brian C. Cowling", _
-                    .Department = "Marketing", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 7, _
-                    .Name = "Thomas C. Dawson", _
-                    .Department = "Marketing", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 8, _
-                    .Name = "Angel M. Wilson", _
-                    .Department = "Marketing", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 9, _
-                    .Name = "Bryan R. Henderson", _
-                    .Department = "Marketing", _
-                    .Position = "Manager" _
-                })
-
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 10, _
-                    .Name = "Harold S. Brandes", _
-                    .Department = "Operations", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 11, _
-                    .Name = "Michael S. Blevins", _
-                    .Department = "Operations", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 12, _
-                    .Name = "Jan K. Sisk", _
-                    .Department = "Operations", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 13, _
-                    .Name = "Sidney L. Holder", _
-                    .Department = "Operations", _
-                    .Position = "Manager" _
-                })
-
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 14, _
-                    .Name = "James L. Kelsey", _
-                    .Department = "Production", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 15, _
-                    .Name = "Howard M. Carpenter", _
-                    .Department = "Production", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 16, _
-                    .Name = "Jennifer T. Tapia", _
-                    .Department = "Production", _
-                    .Position = "Manager" _
-                })
-
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 17, _
-                    .Name = "Judith P. Underhill", _
-                    .Department = "Finance", _
-                    .Position = "Manager" _
-                })
-                stuff_Renamed.Add(New Employee() With { _
-                    .ID = 18, _
-                    .Name = "Russell E. Belton", _
-                    .Department = "Finance", _
-                    .Position = "Manager" _
-                })
-                Return stuff_Renamed
+                Dim stuff As ObservableCollection(Of Employee) = New ObservableCollection(Of Employee)()
+                stuff.Add(New Employee() With {.ID = 1, .Name = "Gregory S. Price", .Department = "", .Position = "President"})
+                stuff.Add(New Employee() With {.ID = 2, .Name = "Irma R. Marshall", .Department = "Marketing", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 3, .Name = "John C. Powell", .Department = "Operations", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 4, .Name = "Christian P. Laclair", .Department = "Production", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 5, .Name = "Karen J. Kelly", .Department = "Finance", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 6, .Name = "Brian C. Cowling", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 7, .Name = "Thomas C. Dawson", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 8, .Name = "Angel M. Wilson", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 9, .Name = "Bryan R. Henderson", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 10, .Name = "Harold S. Brandes", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 11, .Name = "Michael S. Blevins", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 12, .Name = "Jan K. Sisk", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 13, .Name = "Sidney L. Holder", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 14, .Name = "James L. Kelsey", .Department = "Production", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 15, .Name = "Howard M. Carpenter", .Department = "Production", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 16, .Name = "Jennifer T. Tapia", .Department = "Production", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 17, .Name = "Judith P. Underhill", .Department = "Finance", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 18, .Name = "Russell E. Belton", .Department = "Finance", .Position = "Manager"})
+                Return stuff
             End Function
         End Class
 
         Public Sub New()
-            InitializeComponent()
-            gridControl.ItemsSource = Stuff.GetStuff()
+            Me.InitializeComponent()
+            Me.gridControl.ItemsSource = Stuff.GetStuff()
         End Sub
 
         Private Overloads Sub OnDragEnter(ByVal sender As Object, ByVal e As DragEventArgs)
             If e.Data.GetDataPresent(GetType(RecordDragDropData)) Then
-                Dim textBlock As TextBlock = DirectCast(sender, TextBlock)
+                Dim textBlock As TextBlock = CType(sender, TextBlock)
                 textBlock.Background = Brushes.SkyBlue
                 e.Handled = True
             End If
         End Sub
 
         Private Overloads Sub OnDragLeave(ByVal sender As Object, ByVal e As DragEventArgs)
-            Dim textBlock As TextBlock = DirectCast(sender, TextBlock)
+            Dim textBlock As TextBlock = CType(sender, TextBlock)
             textBlock.Background = Nothing
             textBlock.DataContext = Nothing
             e.Handled = True
@@ -167,15 +75,15 @@ Namespace How_to_Drag_and_Drop_Between_GridControl_and_Other_Controls
 
         Private Overloads Sub OnDrop(ByVal sender As Object, ByVal e As DragEventArgs)
             If e.Data.GetDataPresent(GetType(RecordDragDropData)) Then
-                Dim textBlock As TextBlock = DirectCast(sender, TextBlock)
+                Dim textBlock As TextBlock = CType(sender, TextBlock)
                 Dim data As Object = e.Data.GetData(GetType(RecordDragDropData))
-                Dim employees() As Object = DirectCast(data, RecordDragDropData).Records
-                textBlock.DataContext = CType(DirectCast(employees, IList)(0), Employee)
+                Dim employees As Object() = CType(data, RecordDragDropData).Records
+                textBlock.DataContext = CType(CType(employees, IList)(0), Employee)
             End If
         End Sub
 
         Private Overloads Sub OnPreviewMouseLeftButtonDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-            Dim textBlock As TextBlock = DirectCast(sender, TextBlock)
+            Dim textBlock As TextBlock = CType(sender, TextBlock)
             If textBlock.DataContext IsNot Nothing Then
                 DragDrop.DoDragDrop(textBlock, textBlock.DataContext, DragDropEffects.Move)
             End If
@@ -191,7 +99,7 @@ Namespace How_to_Drag_and_Drop_Between_GridControl_and_Other_Controls
         Private Sub OnDropRecord(ByVal sender As Object, ByVal e As DropRecordEventArgs)
             If e.IsFromOutside AndAlso e.Data.GetDataPresent(GetType(Employee)) Then
                 Dim data As Object = e.Data.GetData(GetType(Employee))
-                Dim employee As Employee = DirectCast(data, Employee)
+                Dim employee As Employee = CType(data, Employee)
                 e.Data.SetData(New RecordDragDropData(New List(Of Employee)() From {employee}.ToArray()))
             End If
         End Sub
